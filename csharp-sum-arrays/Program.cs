7a53e5c60c0522230c288d17d217a58e4981ddc9
R-G2110 <argeldelacruz86@gmail.com>
2024-04-17 00:01:24 +0200

    Aggiungere i file di progetto.

---
+++ csharp-sum-arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sum_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = [1, 5.2, 4, 0, -1];
+            double sum = 0;
+
+            foreach (double num in arr)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
