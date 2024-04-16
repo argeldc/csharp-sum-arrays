9dfae12dcd9e2b2686e9be11d98ddaab2337708e
R-G2110 <argeldelacruz86@gmail.com>
2024-04-17 00:24:30 +0200

    [Add]: comments

--- csharp-sum-arrays/Program.cs
+++ csharp-sum-arrays/Program.cs
@@ -6,14 +6,20 @@ namespace sum_arrays
     {
         static void Main(string[] args)
         {
+            
+            //dichiarazione array e contenitore somma
             double[] arr = [1, 5.2, 4, 0, -1];
             double sum = 0;
 
+            //ciclo l'array con foreach
             foreach (double num in arr)
-            {
+            {   
+                //somma ogni elemento dell'array nel contenitore
                 sum += num;
+
             }
 
+            //stampa la somma
             Console.WriteLine(sum);
         }
     }
